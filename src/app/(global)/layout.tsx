4ea459ab53ad7f488bccfb1b2456b769e6100c1a
Roberto Cruz <52827645+spikeboom@users.noreply.github.com>
2025-02-23 16:43:30 -0400

    change to gtm server

--- src/app/(global)/layout.tsx
+++ src/app/(global)/layout.tsx
@@ -79,8 +79,9 @@ export default function RootLayout({
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MDCNTF84');
+            'https://gtm.lovecosmetics.com.br/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NHRJJGG7
+');
           `}
         </Script>
       </head>
@@ -90,7 +91,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MDCNTF84"
+            src="https://gtm.lovecosmetics.com.br/ns.html?id=GTM-NHRJJGG7"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
