e99c8465af5499a16f12692b36650e3dfd419c11
Roberto Cruz <52827645+spikeboom@users.noreply.github.com>
2025-02-23 18:04:31 -0400

    change id

--- src/app/(global)/layout.tsx
+++ src/app/(global)/layout.tsx
@@ -80,7 +80,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://gtm.lovecosmetics.com.br/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MDCNTF84');
+            })(window,document,'script','dataLayer','GTM-NHRJJGG7');
           `}
         </Script>
       </head>
@@ -90,7 +90,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://gtm.lovecosmetics.com.br/ns.html?id=GTM-MDCNTF84"
+            src="https://gtm.lovecosmetics.com.br/ns.html?id=GTM-NHRJJGG7"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
