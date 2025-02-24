d0e817437c8f6af906598fcfcab56e858b316447
Roberto Cruz <52827645+spikeboom@users.noreply.github.com>
2025-02-24 11:05:19 -0400

    add gtm

--- src/app/(global)/layout.tsx
+++ src/app/(global)/layout.tsx
@@ -11,6 +11,7 @@ import { MeuContextoProvider } from "../../components/context/context";
 import Script from "next/script";
 import { Cabecalho } from "./pdp/[slug]/cabecalho/cabecalho";
 import { Rodape } from "./pdp/[slug]/rodape/rodape";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,49 +55,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics */}
-        <Script
-          id="google-analytics-script"
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-SXLFK0Y830`}
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-                      window.dataLayer = window.dataLayer || [];
-                      function gtag(){dataLayer.push(arguments);}
-                      gtag('js', new Date());
-                      gtag('config', 'G-SXLFK0Y830', { debug_mode: true });
-                    `,
-          }}
-        />
-
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://gtm.lovecosmetics.com.br/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NHRJJGG7');
-          `}
-        </Script>
+        <GoogleTagManager gtmId="GTM-T7ZMDHZF" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${playfair.variable} ${lato.variable} ${poppins.variable} bg-white text-[#333] antialiased`}
       >
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://gtm.lovecosmetics.com.br/ns.html?id=GTM-NHRJJGG7"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-
         <MeuContextoProvider>
           <Cabecalho />
 
