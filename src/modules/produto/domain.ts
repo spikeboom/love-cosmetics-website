8f26d18e657efd9de071c56488aa4b08d5afa00b
Roberto Cruz <52827645+spikeboom@users.noreply.github.com>
2025-06-02 11:47:28 -0400

    feat: update fetchProdutosForHome_NotKit endpoint to improve filtering logic

--- src/modules/produto/domain.ts
+++ src/modules/produto/domain.ts
@@ -139,7 +139,7 @@ export const fetchProdutosForHome_Kit = async (): Promise<any> => {
 
 export const fetchProdutosForHome_NotKit = async (): Promise<any> => {
   const baseURL = process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337";
-  const endpoint = `${baseURL}/api/produtos?sort=updatedAt:desc&populate[0]=breadcrumbItems&populate[1]=carouselImagensPrincipal.imagem&populate[2]=listaDescricao&populate[3]=o_que_ele_tem&populate[4]=como_usar_essa_formula&populate[5]=duvidas&populate[6]=resultados.itens_resultado&populate[7]=detalhe_notas&populate[8]=avaliacoes&filters[nome][$notContainsi]=Kit&filters[nome][$notContainsi]=-hide`;
+  const endpoint = `${baseURL}/api/produtos?sort=updatedAt:desc&populate[0]=breadcrumbItems&populate[1]=carouselImagensPrincipal.imagem&populate[2]=listaDescricao&populate[3]=o_que_ele_tem&populate[4]=como_usar_essa_formula&populate[5]=duvidas&populate[6]=resultados.itens_resultado&populate[7]=detalhe_notas&populate[8]=avaliacoes&filters[$and][0][nome][$notContainsi]=Kit&filters[$and][1][nome][$notContainsi]=-hide`;
 
   const response = await fetch(endpoint, {
     method: "GET",
