7875ee431587359fa095f75292801b465c4335ff
Roberto Cruz <52827645+spikeboom@users.noreply.github.com>
2025-06-22 20:07:29 -0400

    refactor: correct import path for Reviews component to align with project structure

--- src/components/product/CustomerReviews/avaliacoes-clientes.tsx
+++ src/components/product/CustomerReviews/avaliacoes-clientes.tsx
@@ -4,7 +4,7 @@ import {
   FaRegStarHalfStroke,
   FaStar,
 } from "react-icons/fa6";
-import Reviews from "../reviews/reviews";
+import Reviews from "../Reviews/reviews";
 
 interface AvaliacoesClientesProps {
   nota: number;
