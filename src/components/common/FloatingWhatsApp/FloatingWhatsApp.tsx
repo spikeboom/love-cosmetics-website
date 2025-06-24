fa5184969d71049a853e706cd236522a74a25a36
Roberto Cruz <52827645+spikeboom@users.noreply.github.com>
2025-06-24 17:48:35 -0400

    refactor: update z-index and positioning of FloatingWhatsApp component for improved visibility

--- src/components/common/FloatingWhatsApp/FloatingWhatsApp.tsx
+++ src/components/common/FloatingWhatsApp/FloatingWhatsApp.tsx
@@ -10,7 +10,7 @@ export function FloatingWhatsApp() {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50 ml-1">
+    <div className="z-99 fixed bottom-1 right-6 z-[1000] ml-1">
       {/* Botão principal */}
       <button
         onClick={handleWhatsAppClick}
